472c89c7f2a764dba106b10ac339a128dd9f1207
Volodymir <volodymyr.babak@skynix.co>
2018-04-10 15:18:41 +0300

    WPSES-5 - remove option, add email from option, fix ccf conflict

--- classes/class-sx-smtp.php
+++ classes/class-sx-smtp.php
@@ -11,17 +11,32 @@ class SX_SMTP
         'port'       => 'sx_smtp_port',
         'username'   => 'sx_smtp_username',
         'password'   => 'sx_smtp_password',
-        'enabled'    => 'sx_smtp_enabled',
+        'from_email' => 'sx_smtp_from_email',
     ];
 
     public function __construct() {
         $this->settings = new SX_SMTP_settings( $this->args );
         $this->mailing  = new SX_SMTP_mailing( $this->args );
+        add_filter( 'ccf_email_headers', array( $this, 'sx_smtp_fix_ccf_conflict' ), 99, 5 );
     }
 
     public function init() {
         $this->mailing->init();
         $this->settings->init();
+    }
+
+    /**
+     * Remove mime version to prevent double mime header error that happend in ccf plugin
+     *
+     * @param $headers
+     * @param $form_id
+     * @param $email
+     * @param $form_page
+     * @param $notification
+     */
+    public function sx_smtp_fix_ccf_conflict( $headers, $form_id, $email, $form_page, $notification ){
+        $mime = array_search( 'MIME-Version: 1.0', $headers );
+        if ( $mime !== false ) { unset( $headers[$mime] ); $headers = array_values( $headers ); }
     }
 
 }
